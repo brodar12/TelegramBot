0ca88676333b4a77bfbb592a43fa659fdc3af142
brodar12 <mihhamd@gmail.com>
2019-11-20 17:50:15 +0200

    add scenario4

--- src/main/java/com/bot/scenario/handler/Scenario4.java
+++ src/main/java/com/bot/scenario/handler/Scenario4.java
@@ -2,27 +2,21 @@ package com.bot.scenario.handler;
 
 import com.bot.core.BotCommandExecutor;
 import com.bot.entity.GlobalResponse;
-import com.bot.entity.ResponseMessage;
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.request.DeleteMessage;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class Scenario3 extends BotCommandExecutor implements Step {
+public class Scenario4 extends BotCommandExecutor implements Step {
 
     private Step nextStep;
     private Map<String,String> allComand= new HashMap<>();
     private String messageAction;
 
 
-
     @Override
     public void process(String message, TelegramBot bot, GlobalResponse resp) {
-
-        System.out.println("S3");
+        System.out.println("S4");
 
         if(resp.getResponseCallbackQuery()!=null) {
 
@@ -31,16 +25,25 @@ public class Scenario3 extends BotCommandExecutor implements Step {
                 System.out.println("chatId:"+resp.getResponseCallbackQuery().getChatID()+" MessageId:"+resp.getResponseCallbackQuery().getMessageId());
                 super.deleteMessage(bot,resp.getResponseCallbackQuery().getChatID().toString(),resp.getResponseCallbackQuery().getMessageId());
 
-                System.out.println("S3 Execute command:" + resp.getResponseCallbackQuery().getData());
-                super.displayKeyborad(bot, resp.getResponseCallbackQuery().getChatID(), "Selectati tipul de telefon pentru IOS!!!", allComand);
+                System.out.println("S4 Execute command:" + resp.getResponseCallbackQuery().getData());
+                super.displayKeyborad(bot, resp.getResponseCallbackQuery().getChatID(), "Selectati daca victima are reteli de socilizare!!!", allComand);
                 super.forceUpdate(bot, resp.getResponseCallbackQuery().getUpdateID());
 
 
+            } else {
+                if (nextStep != null) nextStep.process(message, bot, resp);
             }
-            else {nextStep.process(message, bot, resp); }
         }
-        else {  nextStep.process(message, bot, resp); }
+        else { if (nextStep != null) nextStep.process(message, bot, resp); }
+    }
 
+
+    public Step getNextStep() {
+        return nextStep;
+    }
+
+    public void setNextStep(Step nextStep) {
+        this.nextStep = nextStep;
     }
 
     public Map<String, String> getAllComand() {
@@ -49,14 +52,6 @@ public class Scenario3 extends BotCommandExecutor implements Step {
 
     public void setAllComand(Map<String, String> allComand) {
         this.allComand = allComand;
-    }
-
-    public Step getNextStep() {
-        return nextStep;
-    }
-
-    public void setNextStep(Step nextStep) {
-        this.nextStep = nextStep;
     }
 
     public String getMessageAction() {
